5644ac1c95bc606062885162b93de80934099962
apratt3377 <apratt3377@gmail.com>
2023-07-25 09:00:27 -0400

    remove unnecessary files

--- src/services/common/signatures/ebsi.ts
+++
@@ -1,8 +0,0 @@
-
-//ebsi spec specific signing criteria
-//based on https://ec.europa.eu/digital-building-blocks/wikis/display/EBSIDOC/E-signing+and+e-sealing+Verifiable+Credentials+and+Verifiable+Presentations#EsigningandesealingVerifiableCredentialsandVerifiablePresentations-HowtosignVCandVP
-
-//instead of using did-jwt-vc library, require certain jwt attributes exist for VC and VP
-//might require harder restriction for credentialPayload input...can define type extension here
-
-//export type EBSICredentialPayload extends CredentialPayload
