4dc6ecc9c43401f728eedd0b41f500930f12c8ae
Buster Townsend <btownsend@anonyome.com>
2023-10-06 12:54:16 -0600

    create a JSONLD credential with accompanying tests

--- src/services/common/signatures/jsonld.ts
+++ src/services/common/signatures/jsonld.ts
@@ -1,94 +1,123 @@
-import { CredentialPayload, CreateCredentialOptions, PresentationPayload, CreatePresentationOptions } from "did-jwt-vc";
-import { DIDWithKeys } from "../did/did";
-import { SignatureService } from "./signatures";
-import { Ed25519Signature2018, Ed25519VerificationKey2018 } from "@transmute/ed25519-signature-2018";
+import axios from 'axios';
+import {
+    CredentialPayload,
+    CreateCredentialOptions,
+    PresentationPayload,
+    CreatePresentationOptions,
+} from 'did-jwt-vc';
+import {
+    Ed25519Signature2018,
+    Ed25519VerificationKey2018,
+} from '@transmute/ed25519-signature-2018';
 import { verifiable } from '@transmute/vc.js';
-import { KeyUtils } from "../../../utils";
-import { Did, Url, documentLoaderFactory } from "@transmute/jsonld-document-loader";
-import axios from "axios";
+import { Url, documentLoaderFactory } from '@transmute/jsonld-document-loader';
+import { VerifiableCredential } from '@transmute/vc.js/dist/types/VerifiableCredential';
+import { DIDWithKeys } from '../did/did';
+import { SignatureService } from './signatures';
+import { KEY_ALG, KeyUtils } from '../../../utils';
 
 export class JSONLDService implements SignatureService {
     /**
-     * 
+     *
      * @param keys - `DIDWithKeys` - the DID and the keypair that will sign the token
-    * @param token - `CredentialPayload` - the Credential object
+     * @param token - `CredentialPayload` - the Credential object
      * @param _configs Currently not supported yet.
      * @returns JSON stringified JSON-LD token
      */
-    async signVC(keys: DIDWithKeys, token: CredentialPayload, _configs?: CreateCredentialOptions | undefined): Promise<string> {
-        const key = await this.createVerificationKey(keys)
-        
-        const credential = {
-            '@context': token["@context"],
-            id: token.id,
-            type: token.type,
-            issuer: token.issuer,
-            issuanceDate: token.issuanceDate instanceof Date 
-                ? token.issuanceDate.toISOString() 
-                : token.issuanceDate,
-            credentialSubject: token.credentialSubject,
+    async signVC(
+        keys: DIDWithKeys,
+        token: CredentialPayload,
+        _configs?: CreateCredentialOptions | undefined,
+    ): Promise<string> {
+        const key = await this.createEd25519VerificationKey(keys);
+
+        const { id, type, issuer, issuanceDate, credentialSubject } = token;
+
+        let credential: VerifiableCredential = {
+            '@context': token['@context'],
+            type,
+            issuer,
+            issuanceDate:
+                issuanceDate instanceof Date
+                    ? issuanceDate.toISOString()
+                    : issuanceDate,
+            credentialSubject,
         };
-        
-        // https://github.com/transmute-industries/verifiable-data/tree/main/packages/jsonld-document-loader
-        const documentLoader = documentLoaderFactory.build({
-            ["https://w3id.org/rebase/v1"]: async (iri: Url) => {
-                const { data } = await axios.get(iri);
-                return data;
-            },
-            ["did:key"]: async (did: Did) => {
-                const endpoint = `https://api.did.actor/api/identifiers/${did}`;
-                const { data } = await axios.get(endpoint);
-                return data.didDocument;
-            },
-            ["did:ethr"]: async (_did: Did) => {
-                throw new Error('did:ethr lacks support for Ed25519 signatures');
-            }
-        });
+
+        if (id !== undefined) {
+            credential = { ...credential, id };
+        }
+
+        const documentLoader = this.createDocumentLoader();
 
         // https://www.npmjs.com/package/@transmute/ed25519-signature-2018
         // https://github.com/transmute-industries/verifiable-data/tree/main/packages/vc.js#transmutevcjs
         const vc = await verifiable.credential.create({
             credential,
-            format: ["vc"],
-            documentLoader: (iri) => { 
-                return documentLoader(iri)
-            }, 
+            format: ['vc'],
+            documentLoader: (iri) => {
+                return documentLoader(iri);
+            },
             suite: new Ed25519Signature2018({
-                key
-            })
+                key,
+            }),
         });
 
-        // Stringify the credential for compability between languages 
+        // Stringify the credential for compability between languages
         // and conform to the SignatureService's function signature
-        return JSON.stringify(vc);
-        
+
+        if (vc.items.length === 0) {
+            throw new Error('There are no items found in the credential.');
+        }
+        return JSON.stringify(vc.items[0]);
     }
-    async signVP(_keys: DIDWithKeys, _token: PresentationPayload, _configs?: CreatePresentationOptions | undefined): Promise<string> {
+    async signVP(
+        _keys: DIDWithKeys,
+        _token: PresentationPayload,
+        _configs?: CreatePresentationOptions | undefined,
+    ): Promise<string> {
         // https://github.com/transmute-industries/verifiable-data/tree/main/packages/vc.js#verify-presentation
-        throw new Error("Method not implemented.");
+        throw new Error('Method not implemented.');
     }
 
     /**
      * Creates a verification key to sign the credential.
-     * 
+     *
      * @param keys `DIDWithKeys` to use in signing the credential.
      * @returns `Ed25519VerificationKey2018` the verification key to sign the credential.
      */
-    async createVerificationKey(keys: DIDWithKeys): Promise<Ed25519VerificationKey2018> {
+    async createEd25519VerificationKey(
+        keys: DIDWithKeys,
+    ): Promise<Ed25519VerificationKey2018> {
         const { did, keyPair } = keys;
         const id = did.split(':').pop();
 
-        const base58Keys = KeyUtils.encodeToBase58(keyPair)
+        if (keys.keyPair.algorithm !== KEY_ALG.EdDSA) {
+            throw new Error(
+                'Key must have EdDSA algorithm to be converted into an Ed25519VerificationKey2018',
+            );
+        }
+
+        const base58Keys = KeyUtils.encodeToBase58(keyPair);
 
         return await Ed25519VerificationKey2018.from({
             id: `${did}#${id}`,
-            type: "Ed25519VerificationKey2018", // Used for legacy and compability; Ed25519Signature2020 is for greenfield
+            type: 'Ed25519VerificationKey2018', // Used for legacy and compability; Ed25519Signature2020 is for greenfield
             controller: did,
             publicKeyBase58: base58Keys.publicKey,
             privateKeyBase58: base58Keys.privateKey,
         });
     }
-    
-    name = 'jsonld'
 
+    createDocumentLoader() {
+        // https://github.com/transmute-industries/verifiable-data/tree/main/packages/jsonld-document-loader
+        return documentLoaderFactory.build({
+            ['https://']: async (iri: Url) => {
+                const { data } = await axios.get(iri);
+                return data;
+            },
+        });
+    }
+
+    name = 'jsonld';
 }
--- tests/unit/signatures/jsonld.test.ts
+++ tests/unit/signatures/jsonld.test.ts
@@ -1,29 +1,33 @@
-import { DEFAULT_CONTEXT, JSONLDService, VERIFIABLE_CREDENTIAL, VERIFIABLE_PRESENTATION } from "../../../src/services/common"
-import { KEY_ALG } from "../../../src/utils"
+import {
+    DEFAULT_CONTEXT,
+    JSONLDService,
+    VERIFIABLE_CREDENTIAL,
+    VERIFIABLE_PRESENTATION,
+} from '../../../src/services/common';
+import { KEY_ALG } from '../../../src/utils';
 
 describe('jsonld utilities', () => {
     const didWithKeys = {
-        did: 'did:ethr:maticmum:0xA765CFD161AA0B6f95cb1DC1d933BFf6FAb0ABeE',
+        did: 'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
         keyPair: {
-            algorithm: KEY_ALG.ES256K,
-            publicKey: '027b942c04885bfdcc2497a9a94b2bdf915483cc2c5b5bffd7e86dcf021d731855',
-            privateKey: '0x40dd06c69267386d198939c64580714e9526cea274f13f76b6b16e431d7caaa9'
-        }
-    }
+            algorithm: KEY_ALG.EdDSA,
+            publicKey:
+                '0x76f11a56051843a758f457c5891bac494056d447f3606e5131648c453d6f30f5',
+            privateKey:
+                '0xb2e15a821fe57b6af467ab4c3aaa264456a14f90bed5cf8a00f013bdbe7177be76f11a56051843a758f457c5891bac494056d447f3606e5131648c453d6f30f5',
+        },
+    };
 
-    const context = [DEFAULT_CONTEXT]
+    const context = [DEFAULT_CONTEXT];
     const credentialSubject = {
-        id: "did:ethr:maticmum:0x5F880a6eB77c12Db2e14F29bfE3b1aaf94C95508",
-        name: "Ollie"
-    }
+        id: 'did:ethr:maticmum:0x5F880a6eB77c12Db2e14F29bfE3b1aaf94C95508',
+        // name: "Ollie" NOTE: not valid? MISSING_PROPERTIES_IN_CONTEXT credentialSubject.name
+    };
     const issuer = {
-        id: didWithKeys.did
-    }
-    const type = [
-        VERIFIABLE_CREDENTIAL,
-        "ProofOfName"
-    ]
-    const issuanceDate = "2023-05-18T17:34:26.000Z"
+        id: didWithKeys.did,
+    };
+    const type = [VERIFIABLE_CREDENTIAL, 'ProofOfName'];
+    const issuanceDate = '2023-05-18T17:34:26.000Z';
 
     const VC_PAYLOAD = {
         '@context': context,
@@ -31,26 +35,97 @@ describe('jsonld utilities', () => {
         issuer,
         type,
         issuanceDate,
-    }
-    const holder = didWithKeys.did
-    const verifiableCredential = ['eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJleHAiOjE3MDQxMzcwMDQsInZjIjp7IkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIl0sInR5cGUiOlsiVmVyaWZpYWJsZUNyZWRlbnRpYWwiLCJQcm9vZk9mTmFtZSJdLCJjcmVkZW50aWFsU3ViamVjdCI6eyJuYW1lIjoiT2xsaWUifSwiY3JlZGVudGlhbFNjaGVtYSI6eyJpZCI6Imh0dHBzOi8vZXhhbXBsZS5vcmcvZXhhbXBsZXMvZGVncmVlLmpzb24iLCJ0eXBlIjoiSnNvblNjaGVtYVZhbGlkYXRvcjIwMTgifSwiY3JlZGVudGlhbFN0YXR1cyI6eyJpZCI6Imh0dHBzOi8vZXhhbXBsZS5lZHUvc3RhdHVzLzI0IiwidHlwZSI6IkNyZWRlbnRpYWxTdGF0dXNMaXN0MjAxNyJ9fSwic3ViIjoiZGlkOmV0aHI6bWF0aWNtdW06MHg1Rjg4MGE2ZUI3N2MxMkRiMmUxNEYyOWJmRTNiMWFhZjk0Qzk1NTA4IiwianRpIjoiZGlkOmV0aHI6bWF0aWNtdW06MHgyMGIxY0JCNTU0MjU5Rjc2MzZGQjQ4NzUzNkExN0UwRTcyMjQ4MzY4IiwibmJmIjoxNjg0NDMxMjY2LCJpc3MiOiJkaWQ6ZXRocjptYXRpY211bToweDQzODMzYWVCYzAxOGVkYzU4RDc3NjViYUI0OUI2MWM2RDFlOWQ1NGYifQ.hX-56L8cspoihl7tNYJwuvqhnW3XRYbJY1Hsu5HAEgJFcZGG-3yD2qCgawzLKT2twf9fcz8nBccbCuiyonUjAg']
+    };
+    const holder = didWithKeys.did;
+    const verifiableCredential = [
+        'eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJleHAiOjE3MDQxMzcwMDQsInZjIjp7IkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIl0sInR5cGUiOlsiVmVyaWZpYWJsZUNyZWRlbnRpYWwiLCJQcm9vZk9mTmFtZSJdLCJjcmVkZW50aWFsU3ViamVjdCI6eyJuYW1lIjoiT2xsaWUifSwiY3JlZGVudGlhbFNjaGVtYSI6eyJpZCI6Imh0dHBzOi8vZXhhbXBsZS5vcmcvZXhhbXBsZXMvZGVncmVlLmpzb24iLCJ0eXBlIjoiSnNvblNjaGVtYVZhbGlkYXRvcjIwMTgifSwiY3JlZGVudGlhbFN0YXR1cyI6eyJpZCI6Imh0dHBzOi8vZXhhbXBsZS5lZHUvc3RhdHVzLzI0IiwidHlwZSI6IkNyZWRlbnRpYWxTdGF0dXNMaXN0MjAxNyJ9fSwic3ViIjoiZGlkOmV0aHI6bWF0aWNtdW06MHg1Rjg4MGE2ZUI3N2MxMkRiMmUxNEYyOWJmRTNiMWFhZjk0Qzk1NTA4IiwianRpIjoiZGlkOmV0aHI6bWF0aWNtdW06MHgyMGIxY0JCNTU0MjU5Rjc2MzZGQjQ4NzUzNkExN0UwRTcyMjQ4MzY4IiwibmJmIjoxNjg0NDMxMjY2LCJpc3MiOiJkaWQ6ZXRocjptYXRpY211bToweDQzODMzYWVCYzAxOGVkYzU4RDc3NjViYUI0OUI2MWM2RDFlOWQ1NGYifQ.hX-56L8cspoihl7tNYJwuvqhnW3XRYbJY1Hsu5HAEgJFcZGG-3yD2qCgawzLKT2twf9fcz8nBccbCuiyonUjAg',
+    ];
 
     const VP_PAYLOAD = {
         '@context': context,
         type: VERIFIABLE_PRESENTATION,
         holder,
-        verifiableCredential
-    }
+        verifiableCredential,
+    };
 
-    fit('SignVC not implemented for jsonld', async () => {
-        const jsonldService = new JSONLDService()
-        await expect(jsonldService.signVC(didWithKeys, VC_PAYLOAD))
-            .rejects.toThrowError("Bogus")
-    })
+    it('SignVC for jsonld using did:key', async () => {
+        const jsonldService = new JSONLDService();
+        const ldProof = await jsonldService.signVC(didWithKeys, VC_PAYLOAD);
+        const parsedResult = JSON.parse(ldProof);
+
+        expect(parsedResult).toEqual({
+            '@context': ['https://www.w3.org/2018/credentials/v1'],
+            type: ['VerifiableCredential', 'ProofOfName'],
+            issuer: {
+                id: 'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
+            },
+            issuanceDate: '2023-05-18T17:34:26.000Z',
+            credentialSubject: {
+                id: 'did:ethr:maticmum:0x5F880a6eB77c12Db2e14F29bfE3b1aaf94C95508',
+            },
+            proof: {
+                type: 'Ed25519Signature2018',
+                created: expect.any(String) as string, // Only need to know field is populated
+                verificationMethod:
+                    'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz#z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
+                proofPurpose: 'assertionMethod',
+                jws: expect.any(String) as string, // Only need to know field is populated as it changes each time it is ran
+            },
+        });
+    });
+
+    it('SignVC for jsonld using did:key with an id', async () => {
+        const jsonldService = new JSONLDService();
+        const ldProof = await jsonldService.signVC(didWithKeys, {
+            ...VC_PAYLOAD,
+            id: didWithKeys.did,
+        });
+        const parsedResult = JSON.parse(ldProof);
+
+        expect(parsedResult).toEqual({
+            '@context': ['https://www.w3.org/2018/credentials/v1'],
+            type: ['VerifiableCredential', 'ProofOfName'],
+            issuer: {
+                id: 'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
+            },
+            id: 'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
+            issuanceDate: '2023-05-18T17:34:26.000Z',
+            credentialSubject: {
+                id: 'did:ethr:maticmum:0x5F880a6eB77c12Db2e14F29bfE3b1aaf94C95508',
+            },
+            proof: {
+                type: 'Ed25519Signature2018',
+                created: expect.any(String) as string, // Only need to know field is populated
+                verificationMethod:
+                    'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz#z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
+                proofPurpose: 'assertionMethod',
+                jws: expect.any(String) as string, // Only need to know field is populated as it changes each time it is ran
+            },
+        });
+    });
+
+    it('fails signVC if using keys not encrypted with EdDSA', async () => {
+        // invalid didWithKeys
+        const invalidDidWithKeys = {
+            did: 'did:key:z6MknTZPNAtKXhYUC51KueL2RmJX6nMhZAbjfzV6LRv17Juz',
+            keyPair: {
+                algorithm: KEY_ALG.ES256K,
+                publicKey:
+                    '0x76f11a56051843a758f457c5891bac494056d447f3606e5131648c453d6f30f5',
+                privateKey:
+                    '0xb2e15a821fe57b6af467ab4c3aaa264456a14f90bed5cf8a00f013bdbe7177be76f11a56051843a758f457c5891bac494056d447f3606e5131648c453d6f30f5',
+            },
+        };
+        const jsonldService = new JSONLDService();
+        await expect(
+            jsonldService.signVP(invalidDidWithKeys, VP_PAYLOAD),
+        ).rejects.toThrowError(Error);
+    });
 
     it('SignVP not implemented for jsonld', async () => {
-        const jsonldService = new JSONLDService()
-        await expect(jsonldService.signVP(didWithKeys, VP_PAYLOAD))
-            .rejects.toThrowError(Error)
-    })
-})
+        const jsonldService = new JSONLDService();
+        await expect(
+            jsonldService.signVP(didWithKeys, VP_PAYLOAD),
+        ).rejects.toThrowError(Error);
+    });
+});
